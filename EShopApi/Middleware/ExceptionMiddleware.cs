9bd9530ec46a7aa7f39effb561003c2664041bff
thinhcp123 <thinhhr12@gmail.com>
2022-08-20 14:59:10 +0700

    add Global Exception Handling

---
+++ EShopApi/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace EShopApi.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _next = next;
+            _logger = logger;
+            _env = env;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = 500;
+
+                var response = new ProblemDetails
+                {
+                    Status = 500,
+                    Detail = _env.IsDevelopment() ? ex.StackTrace?.ToString() : null,
+                    Title = ex.Message
+                };
+
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+
+
+}
--- EShopApi/Program.cs
+++ EShopApi/Program.cs
@@ -1,3 +1,5 @@
+using EShopApi.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +11,7 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
